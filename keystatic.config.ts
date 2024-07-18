c6543e41f64b8bf2d9553b53320b153e292a5e12
Nombiembre <emanuelt.marin9@gmail.com>
2024-07-18 07:37:33 -0500

    updated keystatic config

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,4 +1,4 @@
-import { config, fields, singleton } from "@keystatic/core";
+import { collection, config, fields, singleton } from "@keystatic/core";
 
 export default config({
   storage: {
@@ -8,10 +8,12 @@ export default config({
       name: "zap-keystatic",
     },
   },
-  singletons: {
-    test: singleton({
-      label: "test",
-      path: "src/content/posts/",
+  collections: {
+    posts: collection({
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*",
+      format: { contentField: "content" },
       schema: {
         title: fields.slug({ name: { label: "Title" } }),
         content: fields.markdoc({ label: "Content" }),
